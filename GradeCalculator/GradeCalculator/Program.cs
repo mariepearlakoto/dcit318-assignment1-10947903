5e880726dc894a7eb108d26e114f5708d6c001c5
mariepearlakoto <mynakoto@st.ug.edu.gh>
2024-06-01 18:26:25 +0000

    Grade calculator done

--- GradeCalculator/GradeCalculator/Program.cs
+++ GradeCalculator/GradeCalculator/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace GradeCalculator;
+﻿using System;
+
+namespace GradeCalculator;
 
 class Program
 {
-    static void Main(string[] args)
+    static void  Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to Grade Calculator!");
+
+        Console.Write("Enter your grade (0-100): ");
+
+        try
+        {
+            int usersgrade = int.Parse(Console.ReadLine());
+
+            char letterGrade = GetLetterGrade(usersgrade);
+
+            Console.WriteLine("Your letter grade is : " + letterGrade);
+        }
+
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid grade. Enter a number from 0 to 100");
+        }
+
+    }
+
+    static char GetLetterGrade(int usersgrade)
+    {
+
+        if (usersgrade >= 90) return 'A';
+
+        else if (usersgrade >= 80) return 'B';
+
+        else if (usersgrade >= 70) return 'C';
+
+        else if (usersgrade >= 60) return 'D';
+
+        else return 'F';
+
     }
 }
 
